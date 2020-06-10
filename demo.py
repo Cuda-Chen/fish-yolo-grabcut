68e51fef2f6cceabc6c3c687dbed94b725c67121
Cuda-Chen <clh960524@gmail.com>
2020-06-10 16:06:49 +0800

    streamlit demo output test completed
    Streamlit demo output test completed. Reminder of doing these two
    things:
    
    1. Output the results on web.
    2. Deploy on Heroku.

--- demo.py
+++ demo.py
@@ -4,12 +4,23 @@ import cv2 as cv
 from utils import yolo, GrabCut
 import numpy as np
 import streamlit as st
-from PIL import Image
+
+yolopath = "./yolo-fish"
+confidence = 0.25
+threshold = 0.45
 
 st.title("fish YOLO + GrabCut demo")
 
 uploaded_img = st.file_uploader("Choose an image...", type=['png', 'jpg', 'bmp'])
 if uploaded_img is not None:
     file_bytes = np.asarray(bytearray(uploaded_img.read()), dtype=np.uint8)
-    opencv_image = cv.imdecode(file_bytes, 1)
-    st.image(opencv_image, caption='Uploaded Image', channels="BGR", use_column_width=True)
+    image = cv.imdecode(file_bytes, 1)
+    st.image(image, caption='Uploaded Image', channels="BGR", use_column_width=True)
+
+boxes, idxs = yolo.runYOLOBoundingBoxes_streamlit(image, yolopath, confidence, threshold)
+result_images = GrabCut.runGrabCut(image, boxes, idxs)
+
+st.write("")
+st.write("finish grabcut")
+for i in range(len(result_images)):
+    cv.imwrite(f'grabcut{i}.jpg', result_images[i])
