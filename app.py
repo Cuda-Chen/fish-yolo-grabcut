6906f9e850945327f0feb6dea000e4a60867ad10
Cuda-Chen <clh960524@gmail.com>
2020-06-17 14:12:16 +0800

    make a message about user-uploaded image
    1. Make a message about user-uploaded image.
    2. Add 'jpeg' image file upload feature.

--- app.py
+++ app.py
@@ -13,10 +13,12 @@ def run():
 
     st.title("fish YOLO + GrabCut demo")
 
-    uploaded_img = st.file_uploader("Choose an image...", type=['png', 'jpg', 'bmp'])
+    uploaded_img = st.file_uploader("Choose an image...", type=['png', 'jpg', 'bmp', 'jpeg'])
     if uploaded_img is not None:
         file_bytes = np.asarray(bytearray(uploaded_img.read()), dtype=np.uint8)
         image = cv.imdecode(file_bytes, 1)
+
+        st.write("This is your uploaded image:")
         st.image(image, caption='Uploaded Image', channels="BGR", use_column_width=True)
 
         boxes, idxs = yolo.runYOLOBoundingBoxes_streamlit(image, yolopath, confidence, threshold)
